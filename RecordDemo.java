ac2c61c9e4e6541d6c8881f7dfc6bdd419451cbd
p_sbingu <p_sbingu@tencent.com>
2026-07-15 19:06:38 +0800

    feat: add Circle record with custom area() method

--- RecordDemo.java
+++ RecordDemo.java
@@ -2,6 +2,13 @@ public class RecordDemo {
     // Basic record - demonstrates auto-generated constructor, accessors, equals, hashCode, toString
     record Person(String name, int age) {}
 
+    // Record with custom method - demonstrates adding behavior to records
+    record Circle(double radius) {
+        public double area() {
+            return Math.PI * radius * radius;
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("=== Java Records Demo ===\n");
 
@@ -17,6 +24,17 @@ public class RecordDemo {
         System.out.println("person1.equals(person2): " + person1.equals(person2));
         System.out.println("person1.equals(person3): " + person1.equals(person3));
         System.out.println("person1.hashCode() == person2.hashCode(): " + (person1.hashCode() == person2.hashCode()));
+        System.out.println();
+
+        // Record with Custom Method Example
+        System.out.println("2. Record with Custom Method (Circle):");
+        Circle circle1 = new Circle(5.0);
+        Circle circle2 = new Circle(5.0);
+
+        System.out.println("circle1: " + circle1);
+        System.out.println("circle1.radius(): " + circle1.radius());
+        System.out.println("circle1.area(): " + circle1.area());
+        System.out.println("circle1.equals(circle2): " + circle1.equals(circle2));
         System.out.println();
     }
 }
