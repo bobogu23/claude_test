0cbe7b06cdf9e55fb1b11c5f161602624103c5bd
p_sbingu <p_sbingu@tencent.com>
2026-07-16 10:58:29 +0800

    feat: add subtract() method with hard-coded values (10 - 5)

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,13 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        subtract();
+    }
+
+    public static void subtract() {
+        int num1 = 10;
+        int num2 = 5;
+        int result = num1 - num2;
+        System.out.println(num1 + "-" + num2 + "=" + result);
     }
 }
