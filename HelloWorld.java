1d5e63f19f3f16a8486c9f963f6023b5f8dd5658
p_sbingu <p_sbingu@tencent.com>
2026-07-16 11:07:53 +0800

    feat: add addition feature with add() and formatAddition() methods
    - Implement add(int num1, int num2) for integer addition
    - Implement formatAddition(int num1, int num2, int result) for formatted output
    - Update main() to demonstrate with output format {num1}+{num2}={result}
    - Example: 5+3=8

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,14 @@
 public class HelloWorld {
+    public static int add(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static String formatAddition(int num1, int num2, int result) {
+        return String.format("%d+%d=%d", num1, num2, result);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int result = add(5, 3);
+        System.out.println(formatAddition(5, 3, result));
     }
 }
